ffda7d2d899c5c0a5a85260bd05bc60a452b428e
mcbobby <madumcbobby@yahoo.com>
2019-09-18 21:35:34 +0100

    add graphql and first schema

---
+++ schema/schema.js
@@ -0,0 +1,12 @@
+const grapgql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = grapgql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
