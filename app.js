664a1a2598a04f8553da1eee0e50dac8be9ae220
mcbobby <madumcbobby@yahoo.com>
2019-09-18 21:27:39 +0100

    added my rootquery to the schema and used it to define my endpoint schema

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const graphqlHTTP = require('express-graphql');
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    
+
 }))
 
 app.listen(4000, () => {
-    console.log('listening in port 4000');
+    console.log('listening in port 5000');
 })
--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,6 @@
 const grapgql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = grapgql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = grapgql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,4 +9,21 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //get data from DB 
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
